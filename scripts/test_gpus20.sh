e3c91f70aed666fc6622e292da4407e5be6fbb3a
bjzhb666 <bjzhb666@outlook.com>
2024-07-17 10:12:21 +0800

    update scripts

--- scripts/test_gpus20.sh
+++ scripts/test_gpus20.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-bash tools/dist_test.sh configs/segnext/segnext_instance20.py \
+bash tools/dist_test.sh configs/segnext/segnext_instance20_tiny.py \
  work_dirs/0604smallerclassweightdilate5/iter_40000.pth 8 \
     --cfg-options test_dataloader.batch_size=4 val_dataloader.batch_size=4 default_hooks.logger.interval=1 \
     --show-dir work_dirs/debug
--- scripts/train_gpus.sh
+++ scripts/train_gpus.sh
@@ -4,6 +4,6 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #      configs/HLT/HLT_mscan-t_1xb8-adamw-160k_satellite-2048x2048-weight-1-20-seg.py \
 #      8  --work-dir work_dirs/cut_pic1dilate_HLT_mscan-t_1xb8-adamw-160k_satellite-2048x2048-weight-1-20-seg 
 bash tools/dist_train.sh \
-     configs/segnext/segnext_instance20.py \
-     8  --work-dir work_dirs/new \
-     --cfg-options default_hooks.checkpoint.interval=20000  train_cfg.val_interval=100000 
+     configs/segnext/segnext_instance20_small.py \
+     8  --work-dir work_dirs/new20-small-240k \
+     --cfg-options default_hooks.checkpoint.interval=60000 
